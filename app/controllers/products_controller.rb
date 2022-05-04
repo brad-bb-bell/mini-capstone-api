00c165fac63dfef326df04b78347215fd738ea7d
Brad Bell <brad.bb.bell@gmail.com>
2022-05-04 14:29:25 -0600

    Adds update action and route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,18 @@ class ProductsController < ApplicationController
       price: params["price"],
     )
     product.save
-    render product.as_json
+    render json: product.as_json
+  end
+
+  def update
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    product.name = params["name"] || product.name
+    product.description = params["description"] || product.description
+    product.image_url = params["image_url"] || product.image_url
+    product.price = params["price"] || product.price
+    product.save
+    render json: product.as_json
+    # render json: { message: "we updated it!" }
   end
 end
