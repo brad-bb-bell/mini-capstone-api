528d5dae398f2b0423bca246279045de59b61bc8
Brad Bell <brad.bb.bell@gmail.com>
2022-05-04 13:24:23 -0600

    Adds create action with static input

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: "New thing",
+      description: "This thing is sweet",
+      image_url: "www.test.com/test.jpg",
+      price: 99,
+    )
+    product.save
+    render product.as_json
+  end
 end
