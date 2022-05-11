6936d2de8ec4300d350c642581eb243e1ec82090
Brad Bell <brad.bb.bell@gmail.com>
2022-05-11 15:01:39 -0600

    Adds Suppliers model/seeds/routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
+  get "/suppliers" => "suppliers#index"
+  get "/suppliers/id" => "suppliers#show"
+  post "/suppliers" => "suppliers#create"
+  patch "/suppliers/:id" => "suppliers#update"
+  delete "/suppliers/:id" => "suppliers#destroy"
+  #created routes but need to work on controller
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,9 @@ Product.create(name: "Backpacking tent", description: "A small, lightweight tent
 Product.create(name: "Sleeping bag", description: "Warm and packable sleeping bag", image_url: "https://cdn.shopify.com/s/files/1/1416/5622/products/1_71378391-fa39-4d3e-a7ba-7c0cf8e8a064_1080x.jpg?v=1622719073", price: 299.99, quantity: 1)
 
 # Product.create(name: "test", description: "not too short", image_url: "example.com", price: 489.99, quantity: 1)
+
+Supplier.create(name: "Mongoose", email: "supplier@mongoose.com", phone_number: 4065555555)
+
+Supplier.create(name: "Nemo", email: "supplier@nemo.com", phone_number: 9705555555)
+
+Supplier.create(name: "Eolus", email: "supplier@eolus.com", phone_number: 3035555555)
